b77f4634855f55789e0cd5b5a797e3e59e4e7bd9
Dean Merchant <dean.merchant@softwire.com>
2016-01-27 17:14:00 +0000

    Added health check endpoitn & checks file for deploy.

--- src/server/lunch-server.js
+++ src/server/lunch-server.js
@@ -27,6 +27,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(require('webpack-hot-middleware')(compiler, { noInfo: true }));
 }
 
+app.get('status', (req, res) => { res.send('hungry'); });
+
 app.get('/*', (req, res) => {
   res.sendFile('/index.html', { root: __dirname });
 });
