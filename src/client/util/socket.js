0292742acd860515d6b38ced2f5dccc7a0bdf44a
Dean Merchant <dean.merchant@softwire.com>
2016-01-28 17:47:19 +0000

    More port based pain

--- src/client/util/socket.js
+++ src/client/util/socket.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 import { Action } from '../../shared/constants/WeboscketMessageTypes';
 
-const serverPort = __PORT__ || 3333;
+const serverPort = __DEVELOPMENT__ ? __PORT__ || 3333 : 80;
 console.log('SERVER PORT', serverPort);
 
 export const socket = io.connect(`http://${window.location.hostname}:${serverPort}`);
