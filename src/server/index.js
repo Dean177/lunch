c011929d031804729491bb5332214a109a476fb9
Dean Merchant <dean.merchant@softwire.com>
2016-01-28 10:32:39 +0000

    Allow port to be specified via environment config. Dont generate source-maps in production.

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
-require('source-map-support').install();
+if (process.env.NODE_ENV == 'production') {
+  require('source-map-support').install();
+}
 import debug from 'debug';
 import { serverPort } from '../shared/constants/config';
 import lunchServer from './lunch-server';
 
-lunchServer.listen(serverPort,
+lunchServer.listen(process.env.PORT || serverPort,
   () => { debug('lunch:index')(`Lunch listening on port ${serverPort}`); }
 );
