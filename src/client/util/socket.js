d216a6ed79ca2e1dcd6f63f08039c9874be87ae8
Dean Merchant <dean.merchant@softwire.com>
2016-01-29 11:46:50 +0000

    Removed console statement

--- src/client/util/socket.js
+++ src/client/util/socket.js
@@ -2,8 +2,6 @@ import io from 'socket.io-client';
 import { Action } from '../../shared/constants/WeboscketMessageTypes';
 
 const serverPort = __DEVELOPMENT__ ? __PORT__ || 3333 : 80;
-console.log('SERVER PORT', serverPort);
-
 export const socket = io.connect(`http://${window.location.hostname}:${serverPort}`);
 export default function send(action) {
   socket.emit(Action, action);
