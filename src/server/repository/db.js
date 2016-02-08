07da922f7e21b17530103af19a4f02ca7ecd48ef
Dean Merchant <deanmerchant@gmail.com>
2016-02-08 00:19:23 +0000

    remove logging from ci. TBC

--- src/server/repository/db.js
+++ src/server/repository/db.js
@@ -1,11 +1,4 @@
-require('dotenv').config({ path: './ENV', silent: false });
-const logger = require('../../../logger-config');
-logger.warn("dbConnectionString", process.env.DATABASE_URL);
 const config = require('../../../knexfile');
 const db = require('knex')(config);
-
-db.migrate.latest().then(() => {
-  logger.info('Migrations completed successfully');
-}).catch(logger.error);
 
 module.exports = db;
